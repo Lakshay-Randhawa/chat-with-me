8ea16b43276cb3164d1d35bd89c9728b47aaea30
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-08-27 12:15:09 -0600

    React-router-dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 import { StyledEngineProvider } from "@mui/material/styles";
 
 export const App = () => {
   return (
     <StyledEngineProvider injectFirst>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </StyledEngineProvider>
   );
 };
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,16 @@
 import { Navbar } from "../components/Navbar";
+import HomepageImage from "../assets/homepage.webp";
 
 export const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex">Image Login Form</div>
+      <div className="flex p-16">
+        <div className="self-auto basis-9/12">
+          <img src={HomepageImage} alt="Homepage Image" />
+        </div>
+        <div className="basis-3/12">Login Form</div>
+      </div>
     </div>
   );
 };

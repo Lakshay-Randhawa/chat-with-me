d95021c64e3ae7327f3005856f0edf1a2359e90b
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-08-27 12:45:20 -0600

    Navbar architecture changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { StyledEngineProvider } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
+import { Navbar } from "./components/Navbar";
 
 export const App = () => {
   return (
     <StyledEngineProvider injectFirst>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </StyledEngineProvider>
   );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,9 @@
-import { Navbar } from "../components/Navbar";
 import HomepageImage from "../assets/homepage.webp";
-import LoginForm from "../components/LoginForm";
+import { LoginForm } from "../components/LoginForm";
 
 export const Homepage = () => {
   return (
     <div>
-      <Navbar />
       <div className="flex p-16">
         <div className="self-auto basis-9/12">
           <img src={HomepageImage} alt="Homepage Image" />

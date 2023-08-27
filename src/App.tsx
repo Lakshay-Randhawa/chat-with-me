594a1ecd01e54cbbb0c0e8df354c271cdd8e7568
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-08-27 01:45:24 -0600

    Tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { Homepage } from "./pages/Homepage";
 
-function App() {
-  return <div>Homepage</div>;
-}
+export const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
 
 export default App;

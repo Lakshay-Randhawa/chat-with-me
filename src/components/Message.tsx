407de65d39c80a5ead136d7415bc81e1abc88175
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-09-20 20:00:33 -0600

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,8 +8,11 @@ type Props = {
 export const Message = (props: Props) => {
   const { message, isSenderMessage } = props;
 
+  const messageStyle = `border-2 p-2 rounded-xl flex ${
+    isSenderMessage ? "justify-end" : "justify-start"
+  }`;
   return (
-    <Box component="span" className="border-2 flex justify-end p-2">
+    <Box component="span" className={messageStyle}>
       {message}
     </Box>
   );

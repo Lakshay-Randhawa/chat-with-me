9bdd4ddc10f94342abfd2989be411e4bd1375fa8
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-09-23 23:50:23 -0600

    Testing User render

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -5,10 +5,18 @@ import { useFetchAllUsers } from "../hooks/useFetchAllUsers";
 export const Chat = () => {
   useFetchAllUsers();
 
+  const RecentChats = ["user1", "user2", "user3", "user4", "user5"];
   return (
     <div className="flex h-full p-5">
       <div className="border-r-4 border-black basis-1/5">
         <UserSearchBar />
+        <div className="">
+          {RecentChats.map((user, index) => (
+            <div key={index} className=" flex  justify-center ">
+              {user}
+            </div>
+          ))}
+        </div>
       </div>
       <div className=" basis-4/5 p-4">
         <ChatWIindow />

ade3bc9e9482c58a7adcfab8e8639daa24cae79d
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-10-12 01:17:00 -0600

    Update Chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { ChatWIindow } from "../components/chat/ChatWindow";
 import { PreviousChatTab } from "../components/chat/PreviousChatTab";
 import { UserSearchBar } from "../components/chat/UserSearchBar";
 import { useFetchAllUsers } from "../hooks/useFetchAllUsers";
+import { User } from "types/User";
 
 export const Chat = () => {
-  useFetchAllUsers();
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
+  useFetchAllUsers();
   const RecentChats = ["user1", "user2", "user3", "user4", "user5"];
   return (
     <div className="flex h-full p-5">
@@ -13,15 +17,12 @@ export const Chat = () => {
         <UserSearchBar />
         <div className="flex flex-col gap-3 p-6">
           {RecentChats.map((user, index) => (
-            // <div key={index} className=" flex  justify-center ">
-            //   {user}
-            // </div>
             <PreviousChatTab key={index} name={user} />
           ))}
         </div>
       </div>
       <div className=" basis-4/5 p-4">
-        <ChatWIindow />
+        {isChatOpen ? <ChatWIindow /> : <div>Click on a user to chat</div>}
       </div>
     </div>
   );
